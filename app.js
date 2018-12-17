768af6aabc77c49a4d373e14f818803727e75511
Vitor Martins <vitufernandes@yahoo.com.br>
2018-12-17 22:39:47 +0000

    updated url2

--- app.js
+++ app.js
@@ -17,9 +17,8 @@ var express        = require("express"),
 var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campground"),
     indexRoutes       = require("./routes/index")
-    
-console.log(process.env.DATABASEURL);    
-//mongoose.connect("mongodb://localhost/yelp_camp_v12", { useNewUrlParser: true });
+      
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 //mongoose.connect("mongodb://vitormartinswebdesigner:Skate_board457@ds137404.mlab.com:37404/yelpcampv12", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
